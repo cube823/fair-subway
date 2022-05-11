16159d226dfd0c57fac32cb272e44565dc7890bf
minky yoon <cube823@gmail.com>
2022-05-12 00:22:37 +0900

    기본 data fetch

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,13 @@
-import FramerFlex from "client/component/FramerFlex"
-import { NextPage } from "next"
+import FramerFlex from 'client/component/FramerFlex'
+import { Subway } from 'client/utils/types'
 
-const HomePage = (): JSX.Element => {
-    return (
-        <FramerFlex>
-            fair-subway!
-        </FramerFlex>
-    )
+const HomePage = ({ subways }: { subways: Subway[] }): JSX.Element => {
+  console.log(
+    'subways',
+    subways.filter((s) => s.STATION_CD.startsWith('02')),
+  )
+
+  return <FramerFlex></FramerFlex>
 }
 
 export default HomePage
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
-import HomePage from "client/pages";
+import HomePage from 'client/pages'
+import { fetchOnServer } from 'client/utils/fetcher'
+import { SubwayLineInfo } from 'client/utils/types'
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps = async () => {
+  const response = await fetchOnServer<SubwayLineInfo>(1, 753)
+  console.log('response', response.SearchSTNBySubwayLineInfo.row)
+
+  return { props: { subways: response.SearchSTNBySubwayLineInfo.row } }
+}
 
 export default HomePage
